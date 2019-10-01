89c74192376b0edefec2bd713e04dda274b6280d
elijahmm17 <elijahmm17@gmail.com>
2019-10-01 14:28:16 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -4,3 +4,6 @@ let element = document.createElement('div');
 
 element.innerHTML = 'Hello, DOM!';
 element.style.backgroundColor = '#f9f9f9';
+
+document.body.appendChild(element);
+element.syle.textAlign = 'center';
